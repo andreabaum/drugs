55e4dd71c08aa3229674921915dad2e515143cb0
andrea <andrea@morf.org>
2016-01-18 10:42:43 +0100

    Added few drugs helper

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,7 +1,14 @@
 class Drug < ActiveRecord::Base
   has_many :purchases
+  def purchases_sorted
+    purchases.order(when: :desc).limit(5)
+  end
 
   def current_amount
     purchases.sum(:amount)
+  end
+
+  def ends_at
+    purchases.last.when
   end
 end
