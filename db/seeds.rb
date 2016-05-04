766d50d9bdedffa66562a432d12319f3d4c777e6
andrea <andrea@morf.org>
2016-05-04 17:57:35 +0200

    Added simple seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(name: "Andrea")
+drugs = Drug.create([
+  {name: 'Malox', active: 1, reset_amount: 3, reset_at: Date.today - 1.week},
+  {name: 'Aspirin', active: 1, reset_amount: 10, reset_at: Date.today},
+  {name: 'OKI', active: 1}])
+
+Consumption.create(amount: 1, every_days: 7, when: "10:00", drug: drugs.first, user: user, ends_at: Date.today + 1.year)
